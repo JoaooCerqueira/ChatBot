307a7d67f61e1381874f3073d13062eb467a3367
joaoocerqueira <joao.p.cerqueira@hotmail.com>
2024-03-18 09:14:23 +0000

    versão 1.0

--- chatBot_app_testes.py
+++ chatBot_app_testes.py
@@ -1,29 +1,16 @@
 from openai import OpenAI
 import streamlit as st
 import time
-import os
-import pandas as pd
-import cv2
-from io import BytesIO
-from PIL import Image
-
+from utils.utils import upload_file, upload_chat_history, update_chat_history, process_execution_steps
 
 #TODO: Melhorar o download dos ficheiros:
                 # - Definir um path comum a todos os utilizadores
                 # - Fazer de maneira a que so se clicar no download é que extrai o ficheiro
 
-#TODO: Criar varios chats:
-                # - aplicar um botão para cada chat session
-                # - Criar uma thread para cada chat
-                # - Ajustar a variavel message de modo que continue a imprimir tudo direiro e que posso andar entre chats
-                # e aparecer na mesma o historico de mensagens
-
-#TODO: Criar opção de escolher o assistente que quer usar:
-                # - antes de fazer upload dos files escolher o assistente e so depois fazer upload dos files
-
-#TODO: Criar proprio assistente;
-                # - Opção de criar novo assistente do zero
-
+#TODO: Criar historico de chat:
+                # - Criar variavel para guardar os runs desse chat
+                # - para cada um desses runs ir buscar os step Runs
+                # - para cada um dos step runs guardar na variavel message e impirmir
 
 # =============================== Global variables ================================= #
 
@@ -31,10 +18,7 @@ OPENAI_API_KEY = "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"
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-model = "gtp-3.5-turbo-0125"
-
-thread_id = ""
-assistant_id = "asst_Mt4yGoyIFPK7bEvms5pa6qlR"
+assistant_id = "asst_wyxEi4wVIxDKHheu8o3mu5qU"
 
 # Initialize all the session
 if "file_id_list" not in st.session_state:
@@ -43,149 +27,49 @@ if "file_id_list" not in st.session_state:
         )
     st.session_state.file_id_list = [file.id for file in assistant_files.data]
 
+
 if "stat_chat" not in st.session_state:
     st.session_state.stat_chat = False
+
 
 if "thread_id" not in st.session_state:
     st.session_state.thread_id = None
 
+
 if "thread_list" not in st.session_state:
-    st.session_state.thread_list = []
+    with open("beta_openAI/files/thread_list.txt", "r") as file:
+        lines = file.readlines()
 
-if "current_thread_id" not in st.session_state:
-    st.session_state.current_thread_id = None
+    thread_info = {}
 
-if "chat_id_list" not in st.session_state:
-    st.session_state.chat_id_list = []
+    for line in lines:
+        name, thread_id = line.strip().split(",")
+        thread_info[name.strip()] = thread_id.strip()
 
-if "chat_id" not in st.session_state:
-    st.session_state.chat_id = 0
+    st.session_state.thread_list = thread_info
 
-if "current_chat_id" not in st.session_state:
-    st.session_state.current_chat_id = 0
 
-if 'but_a' not in st.session_state:
-    st.session_state.disabled = False
+if 'but_last_chat' not in st.session_state:
+    st.session_state.but_last_chat = False
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
 # ================================================================================== #
-
 # Set up our front end page
 st.set_page_config(
     page_title="Rangel ChatBot",
     page_icon=":bar_chart:",
-    #layout="wide",
     initial_sidebar_state="expanded",
 )
 
 # initialize css
 with open("beta_openAI/styles.css") as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    testee = st.text_input("teste")
-
-# =============================== Functions definitions ============================== #
-
-def upload_file(file_path):
-    with open(file_path, "rb") as file:
-        response = client.files.create(file=file.read(), purpose="assistants")
-    return response.id
-
-def process_message_with_citations(message):
-    """Extract content and annotations from the message and format citations as footnotes."""
-    print(message)
-    for message in message.content:
-        if message.type == "text":
-            message_content = message.text
-            annotations = (
-                message_content.annotations if hasattr(message_content, "annotations") else []
-            )
-            citations = []
-
-            # Iterate over the annotations and add footnotes
-            for index, annotation in enumerate(annotations):
-                # Replace the text with a footnote
-                message_content.value = message_content.value.replace(
-                    annotation.text, f" [{index + 1}]"
-                )
-            # Add the citation to the list
-                # Iterate over the annotations and add footnotes
-            for index, annotation in enumerate(annotations):
-                # Replace the text with a footnote
-                message_content.value = message_content.value.replace(annotation.text, f' [{index}]')
-                if (file_path := getattr(annotation, 'file_path', None)):
-                    cited_file = client.files.retrieve(file_path.file_id)
-                    cited_file_id = cited_file.id
-                    output_path = "C:/Users/joao.cerqueira/Desktop/projeto - automação/beta_openAI/"+ os.path.basename(cited_file.filename)
-                    write_file_to_temp_dir(cited_file_id, output_path)
-                    citations.append(f'[{index}] File was been created in {output_path}')
-                    
-            full_response = message_content.value + "\n\n" + "\n".join(citations)
-            st.session_state.messages.append(
-                {
-                    "role": "assistant",
-                    "content": full_response,
-                    "type": "text"
-                }
-                )
-            with st.chat_message("assistant"):
-                st.markdown(full_response, unsafe_allow_html=True)
-        elif message.type == "image_file":
-            image_file = message.image_file.file_id
-            image = client.files.content(image_file).content
-            st.session_state.messages.append(
-                {
-                    "role": "assistant",
-                    "content": image,
-                    "type": "image"
-                }
-                )
-            with st.chat_message("assistant"):
-                st.image(image)
-
-def display_messages(run_steps):
-    runstep_dict = {}
-    for i,runstep in enumerate(list(reversed(run_steps.data))):
-        print("==================== RUN STEP " + str(i+1) + " ====================")
-        for key, val in runstep:
-            runstep_dict[key] = val      
-        #print(runstep_dict)
-        
-        tmp_list = list(runstep_dict["step_details"])
-
-        if runstep_dict["type"] == "message_creation":
-            print("Message created")
-            mensage_id = tmp_list[0][1].message_id
-            for message in assistant_messages_for_run:
-                if message.id == mensage_id:
-                    process_message_with_citations(message)
-
-        elif runstep_dict["type"] == "tool_calls":
-            for detail in tmp_list[0][1]:
-                tool_type = detail.type
-                if tool_type == "code_interpreter":
-                    print("Code created")
-                    st.session_state.messages.append(
-                        {
-                            "role": "assistant",
-                            "content": detail.code_interpreter.input,
-                            "type": "code"
-                        }
-                    )
-                    with st.chat_message("assistant"):
-                        st.code(detail.code_interpreter.input, language='python')
-        else:
-            print("ERR: runstep type not recognized")
-
-def write_file_to_temp_dir(file_id, output_path):
-    file_data = client.files.content(file_id)
-    file_data_bytes = file_data.read()
-    with open(output_path, "wb") as file:
-        file.write(file_data_bytes)
-
-# ===================================================================================== #
-
 
 # SideBar - Image 
 st.sidebar.image("images\\Rangel.png")
-
 st.sidebar.divider()
 
 # SideBar - Upload files
@@ -193,16 +77,14 @@ with st.sidebar:
     with st.expander("**Upload files**"):
         file_upload = st.file_uploader(
             "**Upload other files:**",
-            key="file_upload",
+            key="file_upload"
         )
 
         # Upload file button - store the file id
         if st.button("**Upload file**",use_container_width=True) and file_upload is not None:
-            with open(f"{file_upload.name}", "wb") as f:
-                f.write(file_upload.getbuffer())
-            another_file_id = upload_file(f"{file_upload.name}")
-            st.session_state.file_id_list.append(another_file_id)
-            st.write(f"File id: {another_file_id}")
+            file_id = upload_file(client, f"{file_upload.name}")
+            st.session_state.file_id_list.append(file_id)
+            st.write("**File uploaded**")
 
 
 # Display those file ids
@@ -211,48 +93,49 @@ if st.session_state.file_id_list:
     assistant_files = client.beta.assistants.files.list(
         assistant_id= assistant_id
         )
-    
     file_ids = ''
     for file_id in st.session_state.file_id_list:
         file = client.files.retrieve(file_id=file_id).filename
         file_ids += (f"- {file} ") + "\n"
-        if file_id not in assistant_files.data:
-            assistant_file = client.beta.assistants.files.create(
-                assistant_id = assistant_id,
-                file_id = file_id
-            )
+        # if file_id not in assistant_files.data:
+        #     assistant_file = client.beta.assistants.files.create(
+        #         assistant_id = assistant_id,
+        #         file_id = file_id
+        #     )
     st.sidebar.markdown(file_ids)
 
 st.sidebar.divider()
 
-# SideBar - Chat session
-if st.sidebar.button("**New chat**",use_container_width=True, key='but_a'):
-    st.sess 
-    st.session_state.chat_id_list.append(st.session_state.chat_id)
-    st.session_state.chat_id += 1
-    # st.sidebar.success("**Chat session started**")
-    # Display chat session id
-    st.sidebar.write("**History Chat:**")
-    with st.sidebar.expander("**Chat session id:**"):
-        for chat_id in st.session_state.chat_id_list:
-            st.sidebar.button(f"Chat id: {chat_id}",use_container_width=True)
+# SideBar - Chat history
+with st.sidebar:
+    with st.expander("**Chat history:**"):
+        for thread_list, id_thread in reversed(st.session_state.thread_list.items()):
+            st.button(thread_list,use_container_width=True, on_click=upload_chat_history, args=[client,id_thread])
 
 st.sidebar.divider()
 
 # Button to initiate the chat session
-if st.sidebar.button("**Start Chatting**",use_container_width=True):
-    if st.session_state.chat_id == 0:
-        st.session_state.chat_id_list.append(st.session_state.chat_id)
-        st.session_state.chat_id += 1
+if st.sidebar.button("**New chat**",use_container_width=True, key='teste'):
+    if st.session_state.messages != []:
+        st.session_state.messages = []
+
+    st.session_state.messages.append(
+        {
+            "role": "assistant", 
+            "content": "Olá, em que posso ajudar?",
+            "type": "text",
+        }
+    )
+
     if st.session_state.file_id_list:
         st.session_state.stat_chat = True
 
         # Create a new thread for this chat session
         chat_thread = client.beta.threads.create()
         st.session_state.thread_id = chat_thread.id
-        st.write(f"Assistant id: {assistant_id}")
-        st.write(f"Thread id: {chat_thread.id}")
+        update_chat_history(chat_thread.id)
 
+        st.rerun()
     else:
         st.sidebar.warning("**No files found. Please upload a file first.**")
 
@@ -261,17 +144,6 @@ st.title("Rangel ChatBot - Beta 💻")
 
 # Check sessions
 if st.session_state.stat_chat:
-    if "openai_model" not in st.session_state:
-        st.session_state.openai_model = "gtp-3.5-turbo-0125"
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
-            {
-            "role": "assistant", 
-            "content": "Olá, em que posso ajudar?",
-            "type": "text",
-            }
-        ]
-
     # Show existing messages
     for message in st.session_state.messages:
         if message["role"] == "assistant":
@@ -283,11 +155,9 @@ if st.session_state.stat_chat:
                 elif message["type"] == "image":
                     st.image(message["content"])
         else:
-             with st.chat_message(message["role"],avatar="👤"):
+            with st.chat_message(message["role"],avatar="👤"):
                 if message["type"] == "text":
                     st.markdown(message["content"])
-                else :
-                    st.code(message["content"], language="python")
 
     # Chat input for users
     if answer := st.chat_input("Escreve aqui a tua mensagem..."):
@@ -298,8 +168,7 @@ if st.session_state.stat_chat:
                 "type": "text"
             }
         )
-
-        with st.chat_message("user"):
+        with st.chat_message("user",avatar="👤"):
             st.markdown(answer)
 
         # Add the user's message to the thread
@@ -318,48 +187,13 @@ if st.session_state.stat_chat:
         # Show a spinner while the assistant is thinking
         with st.spinner("Waiting for the assistant to answer..."):
             while run.status != "completed":
-                print(run.status)
                 time.sleep(1)
                 run = client.beta.threads.runs.retrieve(
                     thread_id = st.session_state.thread_id,
                     run_id = run.id
                 )
-            print(run.status)
-
-            # Retrieve the run steps
-            run_steps = client.beta.threads.runs.steps.list(
-                thread_id=st.session_state.thread_id,
-                run_id=run.id
-            )
-
-            # Retrieve messages added by the assistant
-            messages = client.beta.threads.messages.list(
-                thread_id=st.session_state.thread_id,
-            )
-        
-            # Process and display assis messages
-            assistant_messages_for_run = [
-                message
-                for message in reversed(messages.data)
-                if message.run_id == run.id and message.role == "assistant"
-            ] 
 
             # Display the run steps
-            display_messages(run_steps)
+            process_execution_steps(client,run.id)
 
-            # for message in assistant_messages_for_run:
-            #     full_response = process_message_with_citations(message=message)
-            #     st.session_state.messages.append(
-            #         {
-            #             "role": "assistant",
-            #             "content": full_response,
-            #         }
-            #     )
-            #     with st.chat_message("assistant"):
-            #         st.markdown(full_response, unsafe_allow_html=True)
-
-# else:   
-#     # Prompt users to start chat
-#     st.warning(
-#         "Please upload more files or click on the 'Start Chat' button to start a chat session."
-#     )
+        st.rerun()
---
+++ utils/utils.py
@@ -0,0 +1,163 @@
+import streamlit as st
+import datetime
+
+def upload_file(client, file_path):
+    response = client.files.create(
+        file=open(file_path, "rb"),
+        purpose="assistants",
+        )
+    return response.id
+
+#TODO: ARRANJAR ESTA FUNCÃO, ACHO QUE NAO PRECISO DAS ANNOTATIONS
+def process_message_with_citations(client, message):
+    """Extract content and annotations from the message and format citations as footnotes."""
+    for message in message.content:
+        if message.type == "text":
+            message_content = message.text
+            annotations = (
+                message_content.annotations if hasattr(message_content, "annotations") else []
+            )
+            citations = []
+
+            # Iterate over the annotations and add footnotes
+            for index, annotation in enumerate(annotations):
+                # Replace the text with a footnote
+                message_content.value = message_content.value.replace(
+                    annotation.text, f" [{index + 1}]"
+                )
+
+            # Add the citation to the list
+                # Iterate over the annotations and add footnotes
+            # for index, annotation in enumerate(annotations):
+                # Replace the text with a footnote
+                # message_content.value = message_content.value.replace(annotation.text, f' [{index}]')
+                # if (file_path := getattr(annotation, 'file_path', None)):
+                #     cited_file = client.files.retrieve(file_path.file_id)
+                #     cited_file_id = cited_file.id
+                #     output_path = "C:/Users/joao.cerqueira/Desktop/projeto - automação/beta_openAI/"+ os.path.basename(cited_file.filename)
+                #     write_file_to_temp_dir(cited_file_id, output_path)
+                #     citations.append(f'[{index}] File was been created in {output_path}')
+
+            full_response = message_content.value + "\n\n" + "\n".join(citations)
+            st.session_state.messages.append(
+                    {
+                        "role": "assistant",
+                        "content": full_response,
+                        "type": "text"
+                    }
+                )
+        elif message.type == "image_file":
+            image_file = message.image_file.file_id
+            image = client.files.content(image_file).content
+            st.session_state.messages.append(
+                    {
+                        "role": "assistant",
+                        "content": image,
+                        "type": "image"
+                    }
+                )
+
+def process_execution_steps(client, run_id):
+     # Retrieve the run steps
+    run_steps = client.beta.threads.runs.steps.list(
+        thread_id=st.session_state.thread_id,
+        run_id=run_id
+    )
+
+    # Retrieve messages added by the assistant
+    messages = client.beta.threads.messages.list(
+        thread_id=st.session_state.thread_id,
+        limit=100
+    )
+        
+    runstep_dict = {}
+    for i,runstep in enumerate(list(reversed(run_steps.data))):
+        print("==================== RUN STEP " + str(i+1) + " ====================")
+        for key, val in runstep:
+            runstep_dict[key] = val      
+        #print(runstep_dict)
+        tmp_list = list(runstep_dict["step_details"])
+
+        if runstep_dict["type"] == "message_creation":
+            print("Message created")
+            mensage_id = tmp_list[0][1].message_id
+            for message in messages:
+                if message.id == mensage_id:
+                    process_message_with_citations(client,message)
+
+        elif runstep_dict["type"] == "tool_calls":
+            for detail in tmp_list[0][1]:
+                tool_type = detail.type
+                if tool_type == "code_interpreter":
+                    print("Code created")
+                    st.session_state.messages.append(
+                        {
+                            "role": "assistant",
+                            "content": detail.code_interpreter.input,
+                            "type": "code"
+                        }
+                    )
+                    # if type == "display":
+                    #     with st.chat_message("assistant"):
+                    #         st.code(detail.code_interpreter.input, language='python')
+        else:
+            print("ERR: runstep type not recognized")
+
+def upload_chat_history(client, thread_id):
+    # Retrieve messages added by the assistant
+    st.session_state.messages = []
+    st.session_state.thread_id = thread_id
+
+    messages = client.beta.threads.messages.list(
+        thread_id=thread_id, limit=100
+    )
+
+    reversed_messages = [
+        message
+        for message in reversed(messages.data)
+    ] 
+
+    user_messages = [
+        message
+        for message in reversed_messages
+        if message.role == "user"
+    ]
+
+    # Retrieve the run and run steps
+    run = client.beta.threads.runs.list(
+        thread_id=st.session_state.thread_id,
+        limit=100
+    )
+    with st.spinner("Retrieving chat history..."):
+        for i,run in enumerate(reversed(run.data)):
+            st.session_state.messages.append(
+                {
+                    "role": "user",
+                    "content": user_messages[i].content[0].text.value,
+                    "type": "text"
+                }
+            )
+            process_execution_steps(client, run.id)
+    
+    if st.session_state.messages == []:
+        st.session_state.messages.append(
+            {
+                "role": "assistant", 
+                "content": "Olá, em que posso ajudar?",
+                "type": "text",
+            }
+        )
+
+    st.session_state.stat_chat = True
+
+def update_chat_history(thread_id):
+    data_hora_atual = datetime.datetime.now()
+    data_atual = data_hora_atual.date()
+    hora_atual = data_hora_atual.time()
+
+    data_atual = datetime.date.today()
+    name = "chat "+ data_atual.strftime("%d-%m-%Y") + " " + hora_atual.strftime("%H:%M:%S")
+    with open("beta_openAI/files/thread_list.txt", "a") as file:
+        file.write("\n" + name + " , " + thread_id)
+    file.close()
+    st.session_state.thread_list[name] = thread_id
